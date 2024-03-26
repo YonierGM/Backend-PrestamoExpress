dbf25147b2b6d7b3197e883bb78a30cf31fbd250
YonierGM <yoniermosquera55@gmail.com>
2024-03-26 04:35:01 -0500

    Restore cliente .2

--- routes/cliente.py
+++ routes/cliente.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, HTTPException, Response, status
 from typing import List
+
 from sqlalchemy import insert
 
 from config.db import conn
@@ -19,7 +20,6 @@ def get_clientes():
     return conn.execute(clientes.select()).fetchall()
 
 # Obtener un cliente por su ID
-# Obtener un cliente por su ID
 @clienteRoutes.get("/clientes/{id}", tags=["clientes"], response_model=Cliente, description="Get a single client by ID")
 def get_cliente(id: int):
     existing_cliente = conn.execute(clientes.select().where(clientes.c.clienteid == id)).first()
@@ -32,7 +32,7 @@ def get_cliente(id: int):
 @clienteRoutes.post("/clientes", tags=["clientes"], response_model=Cliente, description="Create a new client")
 def create_cliente(cliente: Cliente):
     try:
-        new_cliente = {"nombre": cliente.nombre, "apellido": cliente.apellido, "documento": cliente.documento, "email": cliente.email, "celular": cliente.celular, "profesion": cliente.profesion, "ingresosMensuales": cliente.ingresosMensuales, "contrasena": cliente.contrasena,"rolid": cliente.rolid}
+        new_cliente = {"nombre": cliente.nombre, "apellido": cliente.apellido, "documento": cliente.documento, "fecha_nac": cliente.fecha_nac, "direccion": cliente.direccion, "celular": cliente.celular, "email": cliente.email, "rolid": cliente.rolid}
         result = conn.execute(insert(clientes).values(new_cliente))
         new_cliente["clienteid"] = result.inserted_primary_key[0]
         conn.commit()
@@ -47,11 +47,11 @@ def update_cliente(id: int, cliente: Cliente):
     if existing_cliente:
         conn.execute(
             clientes.update()
-            .values(nombre=cliente.nombre, apellido=cliente.apellido, documento=cliente.documento, email=cliente.email, celular=cliente.celular, profesion=cliente.profesion, ingresosMensuales=cliente.ingresosMensuales, contrasena=cliente.contrasena, rolid=cliente.rolid)
+            .values(nombre=cliente.nombre, apellido=cliente.apellido, documento=cliente.documento, fecha_nac=cliente.fecha_nac, direccion=cliente.direccion, celular=cliente.celular, email=cliente.email, rolid=cliente.rolid)
             .where(clientes.c.clienteid == id)
         )
         conn.commit()
-        return Response(status_code=status.HTTP_200_OK, content="Cliente actualizado")
+        return status.HTTP_200_OK
     else:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Cliente not found")
 
